ef7e969326e20f6d471a74dffe05fce58b0d8b76
palani006 <spalani1122@gmail.com>
2026-05-02 15:15:44 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from database import Base, engine
+from router import auth, students
+import models.user     
+import models.student  
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="Student Management API", version="1.0.0")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
+app.include_router(students.router, prefix="/students", tags=["Students"])
+
+@app.get("/")
+def root():
+    return {"message": "Student API is running"}
