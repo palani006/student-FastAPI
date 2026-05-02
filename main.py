07d409bb28a3313f94e9a8ac03b5c4dc14d87785
palani006 <spalani1122@gmail.com>
2026-05-02 15:45:33 +0530

    first commit

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -5,12 +7,17 @@ from database import Base, engine
 from router import auth, students
 import models.user     
 import models.student  
+from dotenv import load_dotenv
+load_dotenv()
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Student Management API", version="1.0.0")
+
+FRONTEND_URL=os.getenv("FRONTEND_URL", "http://localhost:5173")
+print(FRONTEND_URL)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[FRONTEND_URL],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
